7cc1ccc69d5debbbd048b73531e4efced5edd564
Alex Kreimer <alex.kreimer@gmail.com>
2016-01-17 07:23:08 +0200

    Replate homography estimation with a simple linear estimator

--- +estimation/H_inf_nonlin.m
+++ +estimation/H_inf_nonlin.m
@@ -1,7 +1,7 @@
-function R = H_inf_nonlin(K, x1, x2, d, b, thr1, thr2)
+function [H,R_out,inliers_out] = H_inf_nonlin(K, x1, x2, R0)
 
-% x1 interest points in image 1
-% x2 interest points in image 2
+% x1 distant interest points in image 1
+% x2 distant interest points in image 2
 % d  depth of the points
 % b  baseline
 % size(x1)=size(x2)=[2,N]
@@ -15,32 +15,39 @@ if rows==2
     x2 = util.e2h(x2);
 end
 
-inliers = d>thr1*b;
+if nargin < 8
+    % The 3x3 homography such that x2 = H*x1.
+    H = vgg.vgg_H_from_x_lin(x1,x2);
+    
+    R = K\H*K;
+    [U,~,V] = svd(R);
+    R = U*eye(3)*V';
+else
+    R = R0;
+end
 
-x1  = x1(:, inliers);
-x2  = x2(:, inliers);
-
-% The 3x3 homography such that x2 = H*x1.
-[H, inliers_H] = estimation.ransacfithomography_vgg(x1, x2, thr2, 1);
-
-R = K\H*K;
-[U,S,V] = svd(R);
-R = U*eye(3)*V';
 r = vrrotmat2vec(R);
 vt0 = r(1:3)*r(4);
 fun = @(vt) objective1(K,x1,x2,vt);
 [vt,resnorm,result,exitflag] = lsqnonlin(fun,vt0);
 R = vrrotvec2mat(vtheta2r(vt));
+
+if nargout>1
+    R_out = R;
+    if nargout > 2
+        inliers_out = inliers;
+    end
+end
 end
 
 function r = vtheta2r(vtheta)
-    angle = norm(vtheta);
-    if angle == 0
-        axis = [0 1 0];
-    else
-        axis  = vtheta(1:3)/angle;
-    end
-    r = [axis angle];
+angle = norm(vtheta);
+if angle == 0
+    axis = [0 1 0];
+else
+    axis  = vtheta(1:3)/angle;
+end
+r = [axis angle];
 end
 
 function val = objective1(K,x1,x2,vtheta)

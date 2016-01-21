577ea204cecb05aea9f5414d8344ab0c15a347bd
Alex Kreimer <alex.kreimer@gmail.com>
2016-01-21 13:39:56 +0200

    Add distances to the line to output params

--- +estimation/fit_line_e.m
+++ +estimation/fit_line_e.m
@@ -1,9 +1,9 @@
-function l = fit_line_e(p1, p2, e)
+function [l,d1,d2] = fit_line_e(p1, p2, e)
 
-p1 = p1-e;
-p2 = p2-e;
+p1e = p1-e;
+p2e = p2-e;
 
-A = [p1'; p2'];
+A = [p1e'; p2e'];
 
 % min norm(Ax) s.t. norm(x)=1
 [~,~,V] = svd(A);
@@ -14,5 +14,10 @@ n = V(:,end);
 % intercept
 c = -n(1)*e(1)-n(2)*e(2);
 l = [n; c];
+
+if nargout>1
+    d1 = l'*util.e2h(p1);
+    d2 = l'*util.e2h(p2);
+end
 
 end

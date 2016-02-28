bba4cd8ab256f16172c57201660b99d7f12ae743
Alex Kreimer <alex.kreimer@gmail.com>
2016-02-28 19:38:35 +0200

    Merge intro and add eccv template

--- run_all.sh
+++ run_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-sequences="04 00 01 02 03 05 06 07 08 09 10"
+sequences="00 01 02 03 05 06 07 08 09 10"
 depths="150 200"
 inlier_thrs="3 4 5"
 ransac_iters="300 400"
@@ -14,9 +14,8 @@ do
 	    for ransac_iter in $ransac_iters
 	    do
 		output_file="${sequence}_${depth}_${inlier_thr}_$ransac_iter"
-		matlab -r "run_sequence('04','depth_thr',$depth,'inlier_thr',$inlier_thr,'ransac_iter',$ransac_iter);" > $output_file 2>&1 &
+		matlab -r "run_sequence('$sequence','depth_thr',$depth,'inlier_thr',$inlier_thr,'ransac_iter',$ransac_iter);" > $output_file 2>&1 &
 		echo "run $output_file"
-
 	    done
 	done
     done

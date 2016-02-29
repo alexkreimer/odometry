840311ee87855a161c0f2f37a46eeca29f6ec09c
Alex Kreimer <alex.kreimer@gmail.com>
2016-02-29 14:57:45 +0200

    eccv submission update

--- +eval/evaluate_odometry.m
+++ +eval/evaluate_odometry.m
@@ -19,6 +19,10 @@ num_alg= length(fields);
 display = cell(1,length(fields));
 for i = 1:length(fields)
     display{i} = strrep(fields{i},'_',' ');
+    display{i} = strrep(display{i}, 'results', '');
+    if display{i}(end) == 's'
+        display{i} = 'SS';
+    end
 end
 
 num_frames = inf;
@@ -46,6 +50,12 @@ end
 clickableLegend(display);
 %legend(gca,'show'); 
 axis equal;
+xlabel('x [m]');
+ylabel('z [m]');
+width = 16; % cm 
+height = 12; % cm
+set(gcf, 'PaperPosition', [0, 0, width / 2.54, height / 2.54])
+print -dpng -r500 top_path
 
 figure;
 hold on; title('trajectory distances');
@@ -107,24 +117,34 @@ set(gca, 'LineStyleOrder', {'-', ':', '--', '-.'}); % different line styles
 hold all
 
 for k = 1:num_alg
-    field = fields{k};
-    plot(lengths,rotation_errors(k,:), 'DisplayName', [strrep(field,'_',' '),' r error']);
+    plot(lengths,rotation_errors(k,:), 'DisplayName', [display{k}, ' ', num2str(mean(rotation_errors(k,~isnan(rotation_errors(k,:)))))]);
+    leg{k} = [display{k}, ' ', num2str(mean(rotation_errors(k,~isnan(rotation_errors(k,:)))))];
 end
 ylabel('deg/m');
 legend(gca,'show');
-clickableLegend(display);
+
+clickableLegend(leg);
+width = 16; % cm 
+height = 12; % cm
+set(gcf, 'PaperPosition', [0, 0, width / 2.54, height / 2.54])
+print -dpng -r500 rotation_error
 
 figure; hold on; title('translation error');
 set(gca, 'LineStyleOrder', {'-', ':', '--', '-.'}); % different line styles
 % Call hold so that the first plot doesn't affect the properties you just set
 hold all
 for k = 1:num_alg
-    field = fields{k};
-    plot(lengths, translation_errors(k,:), 'DisplayName', [strrep(field,'_',' '),' t error']);
+    plot(lengths, translation_errors(k,:), 'DisplayName', [display{k}, ' ',num2str(mean(translation_errors(k,~isnan(translation_errors(k,:)))))]);
+    leg{k} = [display{k}, ' ',num2str(mean(translation_errors(k,~isnan(translation_errors(k,:)))))];
 end
 ylabel('%');
-clickableLegend(display);
+clickableLegend(leg);
 %legend(gca,'show');
+width = 16; % cm 
+height = 12; % cm
+set(gcf, 'PaperPosition', [0, 0, width / 2.54, height / 2.54])
+print -dpng -r500 translation_error
+
 end
 
 function last_frame = get_last_frame_for_dist(dist, first_frame, len)

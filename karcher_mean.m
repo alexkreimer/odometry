6c61d4ce17e1edf0dc05a37db2318cd02e422248
Alex Kreimer <alex.kreimer@gmail.com>
2016-03-22 11:44:32 +0200

    Remove dead code

--- karcher_mean.m
+++ karcher_mean.m
@@ -8,8 +8,6 @@ while 1
     for i=1:N
         vr = vrrotmat2vec(R'*RR{i})';
         v(:,i) = vr(1:3)*vr(4);
-        
-        %v(:,i) = log_map(R'*RR{i});
     end
     v = mean(v,2);
     if norm(v)<tol
@@ -19,15 +17,3 @@ while 1
 end
 
 end
-
-
-
-function R = exp_map(v)
-
-theta = norm(v);
-V = util.skew(v/theta);
-
-R = eye(3) + sin(theta)*V + (1-cos(theta))*V*V;
-
-end
-
